403498e511b0646f433cb46865b5844586174931
Logan Garbarini <logan@neuralink.com>
2024-02-11 22:28:08 -0800

    Add XIAPI_INCLUDE_DIR for hermetic/cross builds

--- build.rs
+++ build.rs
@@ -5,11 +5,15 @@ use std::path::PathBuf;
 
 fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").expect("Unknown target OS");
-    let (link_lib, include_path) = match target_os.as_str() {
-        "windows" => ("xiapi64","C:/XIMEA/API/xiAPI"),
-        "linux" => ("m3api", "/opt/XIMEA/include"),
-        "macos" => ("m3api","/Library/Frameworks/m3api.framework/Headers"),
+    let (link_lib, mut include_path) = match target_os.as_str() {
+        "windows" => ("xiapi64", "C:/XIMEA/API/xiAPI".to_string()),
+        "linux" => ("m3api", "/opt/XIMEA/include".to_string()),
+        "macos" => ("m3api","/Library/Frameworks/m3api.framework/Headers".to_string()),
         x => panic!("Unknown platform: {x}"),
+    };
+
+    if let Ok(override_include_path) = env::var("XIAPI_INCLUDE_DIR") {
+        include_path = override_include_path;
     };
 
     println!("cargo:rerun-if-changed=wrapper.h");
@@ -22,14 +26,14 @@ fn main() {
     }
 
     if target_os.as_str() == "windows" {
-        println!("cargo:rustc-link-search={}",include_path);
+        println!("cargo:rustc-link-search={}", include_path);
     }
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg(format!("-I{}", include_path))
         .constified_enum_module(".*")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .derive_default(true)
         .generate()
         .expect("Unable to generate bindings");
